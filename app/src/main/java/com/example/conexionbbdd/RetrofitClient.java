1d884065750c475bdcf4379ce852bfbdc7ce072e
kmyte <kmyteran2@hotmail.com>
2025-06-10 16:21:21 +0200

    Cambios en listar reporte

--- app/src/main/java/com/example/conexionbbdd/RetrofitClient.java
+++ app/src/main/java/com/example/conexionbbdd/RetrofitClient.java
@@ -1,24 +1,29 @@
 package com.example.conexionbbdd;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-import retrofit2.converter.gson.GsonConverterFactory; // Añade esto
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static final String BASE_URL = "https://api-cxdc.onrender.com";
 
     private static Retrofit retrofit;
 
-
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+            // Cliente con timeouts extendidos
             OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS) // Tiempo máximo para conectar
+                    .readTimeout(30, TimeUnit.SECONDS)    // Tiempo máximo para leer respuesta
+                    .writeTimeout(30, TimeUnit.SECONDS)   // Tiempo máximo para enviar cuerpo de solicitud
                     .addInterceptor(interceptor)
                     .build();
 
